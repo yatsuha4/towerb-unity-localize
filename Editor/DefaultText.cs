c26ed33e2060d2b851b5d717a34a3ebe0775005b
YASUI Kentarow <yasuikentarow@gmail.com>
2022-02-17 23:30:26 +0900

    整形

--- Editor/DefaultText.cs
+++ Editor/DefaultText.cs
@@ -9,55 +9,56 @@ using UnityEngine.UI;
 
 namespace Towerb.Editor.Localize
 {
-  /**
-     ローカライズされたテキストを設定する
-  */
-  public static class DefaultText {
     /**
-     */
-    [MenuItem("Towerb/Localize/DefaultText")]
-    public static void SetDefaultText()
+       ローカライズされたテキストを設定する
+    */
+    public static class DefaultText
     {
-      foreach(var obj in Resources.FindObjectsOfTypeAll<LocalizeStringEvent>())
-      {
-        if(obj.GetComponent<Text>() is Text text)
+        /**
+         */
+        [MenuItem("Towerb/Localize/DefaultText")]
+        public static void SetDefaultText()
         {
-          SetText(text, "m_text", obj.StringReference);
-        }
-        else if(obj.GetComponent<TextMeshProUGUI>() is TextMeshProUGUI textMesh)
-        {
-          SetText(textMesh, "m_text", obj.StringReference);
-        }
-      }
-    }
-
-    /**
-     */
-    private static void SetText(Object component, string property, LocalizedString src)
-    {
-      if(!src.IsEmpty)
-      {
-        var collection = 
-          LocalizationEditorSettings.GetStringTableCollection(src.TableReference);
-        if(collection != null)
-        {
-          var entry = collection.GetRowEnumerator().
-            First(row => row.KeyEntry.Id == src.TableEntryReference.KeyId);
-          for(int i = 0; i < entry.TableEntriesReference.Length; i++)
-          {
-            var locale = 
-              LocalizationEditorSettings.GetLocale(entry.TableEntriesReference[i].Code);
-            if(locale.SortOrder == 0)
+            foreach(var obj in Resources.FindObjectsOfTypeAll<LocalizeStringEvent>())
             {
-              var serializedObject = new SerializedObject(component);
-              serializedObject.FindProperty(property).stringValue = 
-                entry.TableEntries[i].LocalizedValue;
-              serializedObject.ApplyModifiedProperties();
-              break;
+                if(obj.GetComponent<Text>() is Text text)
+                {
+                    SetText(text, "m_text", obj.StringReference);
+                }
+                else if(obj.GetComponent<TextMeshProUGUI>() is TextMeshProUGUI textMesh)
+                {
+                    SetText(textMesh, "m_text", obj.StringReference);
+                }
             }
-          }
         }
-      }
+
+        /**
+         */
+        private static void SetText(Object component, string property, LocalizedString src)
+        {
+            if(!src.IsEmpty)
+            {
+                var collection = 
+                    LocalizationEditorSettings.GetStringTableCollection(src.TableReference);
+                if(collection != null)
+                {
+                    var entry = collection.GetRowEnumerator().
+                        First(row => row.KeyEntry.Id == src.TableEntryReference.KeyId);
+                    for(int i = 0; i < entry.TableEntriesReference.Length; i++)
+                    {
+                        var locale = 
+                            LocalizationEditorSettings.GetLocale(entry.TableEntriesReference[i].Code);
+                        if(locale.SortOrder == 0)
+                        {
+                            var serializedObject = new SerializedObject(component);
+                            serializedObject.FindProperty(property).stringValue = 
+                                entry.TableEntries[i].LocalizedValue;
+                            serializedObject.ApplyModifiedProperties();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
 }
